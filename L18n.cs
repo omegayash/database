2b419168888a7295f6a278a12306ee1d327048c8
omegayash <variayash@gmail.com>
2024-01-30 11:20:23 +0530

    Add files via upload

---
+++ L18n.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MobileApplicationOFRA.DataAccessLayer;
+using MobileApplicationOFRA.Model.MultilanguageModel;
+using MobileApplicationOFRA.Utility;
+using Plugin.Settings;
+
+namespace MobileApplicationOFRA.MultiLanguage
+{
+    public class L18n
+    {
+        public L18n()
+        {
+        }
+        public static Dictionary<string, string> cache { get; set; }
+        public static List<ResourceBundle> GetlangList { get; set; }
+        /// <remarks>
+        /// Maybe we can cache this info rather than querying every time
+        /// </remarks>
+        public static List<ResourceBundle> GetLang()
+        {
+            var MultiLang = new DatabaseHandler().getItems<ResourceBundle>();
+            return MultiLang;
+        }
+        public static string Localize(string key, string LangID)
+        {
+            //    "pm.screen.key"
+            // Platform-specific
+            //  if(GetlangList==null)
+            // {
+            GetlangList = GetLang();
+            // }
+            if (GetlangList.Any(s => s.languageKey == key && s.language_id == LangID.ToString()))
+            {
+                var result = GetlangList.SingleOrDefault(s => s.languageKey == key && s.language_id == LangID.ToString()).text;
+                return result;
+            }
+            CrossSettings.Current.AddOrUpdateValue("timespan", Convert.ToString(UtilityFunction.ConvertToUnixTimestamp(DateTime.Now)));
+            return key + "???";
+        }
+
+    }
+}
---
+++ TranslateExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApplicationOFRA.MultiLanguage
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        public TranslateExtension()
+        {
+        }
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+
+            // Do your translation lookup here, using whatever method you require
+            var translated = L18n.Localize(Text, App.LangID);
+
+            return translated;
+        }
+    }
+}
